1dac985976b51589602c07bae1c6d26dfe0272d2
shubhamsaini-commits <shubham.saini.dev.cse@gmail.com>
2026-01-05 21:47:37 +0530

    Enhance 07_maxsubarray.cpp with explanatory comments
    Added comments explaining the implementation of Kadane's algorithm for finding the maximum subarray sum.

--- 07_maxsubarray.cpp
+++ 07_maxsubarray.cpp
@@ -1,7 +1,20 @@
+// THE QUESTION.....
+// Given an integer array nums,
+// find the contiguous subarray (containing at least one number) which has the largest sum and return that sum.
+// Input: nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4}
+// Output: 6 --> 4 , , -1  , 2 , 1 gives the maximum sum...
+
+
 #include<iostream>
 #include<vector>
 #include<climits>
 using namespace std;
+
+
+// THIS IS THE MOST OPTIMIZED APPROACH FOR GETTING MAXIMUM SUBARRAY ..
+// KNOWN AS KADANE'S ALGORITHM..
+// THE MAIN IDEA IS JUST KEEP ADDING THE ELEMENTS ... AT ANY MOMENT .. OUR CURRENT SUM GETS NEGATIVE.. SET IT TO ZERO .. 
+// and take maximum of cs and ms.. 
 
 
 int maxSum(vector <int> vec){
@@ -9,8 +22,8 @@ int maxSum(vector <int> vec){
     for (int i = 0; i < vec.size(); i++)
     {
         sum += vec[i];
-        ms = max(sum , ms);
-        if(sum<=0){
+        ms = max(sum , ms);    // THE SIDE CASE... suppose the array only has the negative elements.. 
+        if(sum<=0){            //if we set sum = 0 before the ms = max() our answer will always be 0 .. which is wrong  !! 
             sum=0;
         }
     }
@@ -24,4 +37,5 @@ int main(){
 
 
     return 0;
+
 }
