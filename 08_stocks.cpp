f10934d80e07e96af2bc8f625ef28518d1174dd4
shubhamsaini-commits <shubham.saini.dev.cse@gmail.com>
2026-01-05 21:55:33 +0530

    Refactor stock profit calculation functions
    Refactor maxProfit and maxProfit2 functions for clarity and optimization. Added comments for better understanding of the code.

--- 08_stocks.cpp
+++ 08_stocks.cpp
@@ -1,8 +1,23 @@
+// BEST TIME TO BUY AND SELL STOCK
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a
+// different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// EXAMPLE.. 
+// Input: prices = {7, 1, 5, 3, 6, 4}
+// Output: 5
+
+
 #include<iostream>
 #include<vector>
 #include<climits>
 using namespace std;
 
+
+
+// the bruiteforce approach.. where we ara checking every possible progit and returning the maximum value..
+// Time complexity n squared
 int maxProfit(vector <int> &vec){
     int mp= INT_MIN , cp;
     for (int i = 1; i < vec.size(); i++)
@@ -31,13 +46,13 @@ int maxProfit2(vector <int> &vec){
     int mp =0;
     for (int i = 0; i < vec.size(); i++)
     {
-        if (min<vec[i])
+        if (min<vec[i]) // remember which day the stock was cheapest
         {
             min = vec[i];
         }
         if (vec[i]-min> mp)
         {
-            mp =vec[i]-min;
+            mp =vec[i]-min;  // the maximum profit...
         }
         
     }
@@ -53,4 +68,5 @@ int main(){
 
 
     return 0;
+
 }
