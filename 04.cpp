900d9f70a16ee6fa6820be3a7105698eab03f18b
shubhamsaini-commits <shubham.saini.dev.cse@gmail.com>
2026-01-12 23:23:36 +0530

    Update 04.cpp

--- 04.cpp
+++ 04.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+// find a missing number in a continuous array of 0 - N  {0 ,  1 , 2 , 4 , 5  , 6}
+
 int findMissing(vector <int> vec , int N){
     int sum = N*(N+1)/2;
     int vecsum = 0;
@@ -15,4 +18,5 @@ int main(){
 
 
     return 0;
+
 }
