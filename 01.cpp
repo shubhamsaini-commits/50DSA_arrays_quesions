543fcb2f384b4a5d80c4beb5bf4f25d6365b9d85
shubhamsaini-commits <shubham.saini.dev.cse@gmail.com>
2026-01-12 23:19:29 +0530

    Update 01.cpp

--- 01.cpp
+++ 01.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include <climits>
 using namespace std;
-
+ // find maximum and minimum of a given array .. 
 
 int main(){
     int arr[] = {3, 5, 4, 1, 9};
@@ -21,4 +21,5 @@ int main(){
     cout<<"the maximum is = "<<max;
     cout<<"the minimum is = "<<min;
     return 0;
+
 }
