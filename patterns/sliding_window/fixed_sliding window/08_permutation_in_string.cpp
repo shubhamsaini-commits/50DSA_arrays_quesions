aa00291bcbfa32b57f12993f3f28efe4e547e82e
shubhamsaini-commits <shubham.saini.dev.cse@gmail.com>
2026-01-16 14:02:45 +0530

    Add files via upload

---
+++ patterns/sliding_window/fixed_sliding window/08_permutation_in_string.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+   
+
+bool check_permutation(string &s1 , string &s2){
+    if(s1.size()>s2.size()) return false;
+    vector <int> need(26, 0);
+    vector <int> window(26, 0);
+    int st=0 , end= s1.size()-1;
+    for(char c: s1){
+        need[c - 'a']++;
+    }
+    for(int j =0 ; j< s1.size() ; j++) {
+     window[s2[j]-'a']++;
+    }
+    if(window == need){
+        
+        return true;
+    }
+    
+    
+    for(end = end+1; end<s2.size() ; end++){
+        window[s2[st]-'a']--;
+        st++;
+        
+        window[s2[end] - 'a']++;
+
+        if (window == need)
+        {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+int main(){
+  string s2 = "abbacacd";
+  string s1 = "abc";
+  return 0;
+}
